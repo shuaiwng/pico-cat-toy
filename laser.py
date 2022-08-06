f4a04e0c516f8ce6f2269d2e8cc57c2b7f497103
shuaiwng <shuai.wang2@protonmail.com>
2022-08-07 01:47:48 +0200

    comment the right port

--- laser.py
+++ laser.py
@@ -4,7 +4,7 @@ import utime                                                         #include de
 
 class Laser:
     def __init__(self):
-        self.led_laser = machine.Pin(6, machine.Pin.OUT)                      #define LED pin function:GP15,ouput function
+        self.led_laser = machine.Pin(6, machine.Pin.OUT)                      #define LED pin function:GP6,ouput function
 
     def laser_on(self):
         self.led_laser.value(1)
